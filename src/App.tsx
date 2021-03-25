6cb4efadd09de8521bef3ffd0e452431f236b681
MOMO Tidjani <mty.tidjani@gmail.com>
2021-03-25 08:51:56 +0100

    Dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+const DarkSwitch: React.FC<unknown> = () => {
+  const [theme, setTheme] = useState(localStorage.getItem("theme"));
+
+  useEffect(() => {
+    if (!theme) {
+      const dark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+      if (dark) document.documentElement.classList.add("dark");
+    } else if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    }
+  });
+
+  const toggleDark = () => {
+    document.documentElement.classList.toggle("dark");
+    const newTheme = theme === "dark" ? "light" : "dark";
+    localStorage.setItem("theme", newTheme);
+    setTheme(newTheme);
+  };
+
+  return (
+    <label className="dark-switch">
+      <input checked={theme === "dark"} type="checkbox" onChange={toggleDark} />
+      <span className="slider">🌞</span>
+    </label>
+  );
+};
 
 function App() {
   return (
@@ -18,6 +45,7 @@ function App() {
         >
           Learn React
         </a>
+        <DarkSwitch />
       </header>
     </div>
   );
